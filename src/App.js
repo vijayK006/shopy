ff0c5d62db6abfb6d42cf94f108a56a56fd2e34c
Vijay Kale <105584316+vijayK006@users.noreply.github.com>
2024-05-17 06:17:49 +0000

    pulling code main

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ const App = () => {
       <BrowserRouter>
         <Routes>
           <Route path="/:employeeId" element={<ProtectRoute Component={Home} />} />
+          <Route path="/" element={<ProtectRoute Component={Home} />} />
           {/* <Route path="/" element={<Home/>} /> */}
           <Route path="/login" element={<Login />} />
           <Route path="/employee-login" element={<EmployeeLogin />} />
